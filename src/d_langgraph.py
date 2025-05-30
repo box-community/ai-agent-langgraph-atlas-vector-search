37de2fdc3ca06129508414d35901ad4c85144ab4
Rui Barbosa <barduinor@gmail.com>
2025-05-30 11:59:07 -0400

    remove dead code

--- src/d_langgraph.py
+++ src/d_langgraph.py
@@ -61,20 +61,6 @@ def tools_node(state: GraphState) -> Dict[str, List]:
     return {"messages": result}
 
 
-# # Stream outputs from the graph as they pass through its nodes
-# def execute_graph(user_input: str, app: CompiledStateGraph) -> None:
-#     # Add user input to the messages attribute of the graph state
-#     input = {"messages": [("user", user_input)]}
-#     # Pass input to the graph and stream the outputs
-#     for output in app.stream(input):
-#         for key, value in output.items():
-#             print(f"Node {key}:")
-#             print(value)
-
-#     print("\n---FINAL ANSWER---")
-#     print(value["messages"][-1].content)
-
-
 def execute_graph(thread_id: str, user_input: str, app: CompiledStateGraph) -> None:
     config = {"configurable": {"thread_id": thread_id}}
     input = {
